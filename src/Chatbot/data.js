4202a63c3735151d965d2a02f07e2f0b5e0f5d19
Raman <103361956+Ramanbansal2004@users.noreply.github.com>
2024-12-14 23:09:42 +0530

    chatbot

---
+++ src/Chatbot/data.js
@@ -0,0 +1,40 @@
+const data = {
+    kid: {
+        fast: {
+            name: "Small Pox",
+            imageUrl: "https://ssl.adam.com/graphics/images/en/19059.jpg",
+            link: "https://www.cdc.gov/smallpox/about/index.html#:~:text=While%20some%20antiviral%20drugs%20may,the%20disease%20and%20proven%20effective."
+        },
+        slow: {
+            name: "Naphrotic Syndrome",
+            imageUrl: "https://media.medigac.com/open/Feature-Photos/Hospital%20ward/Paediatrics/Nephrotic%20Syndrome.webp",
+            link: "https://my.clevelandclinic.org/health/diseases/5989-nephrotic-syndrome"
+        }
+    },
+    teenagers: {
+        slow: {
+            name: "Maleria",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHSZmZr1ve6_G3r-Gf3SU88k0Tks3d2SqWCA&s",
+            link: "https://my.clevelandclinic.org/health/diseases/15014-malaria"
+        },
+        fast: {
+            name: "Asthma",
+            imageUrl: "https://aafa.org/wp-content/uploads/2022/08/asthma-education-what-happens-in-lungs-when-you-have-asthma.png",
+            link: "https://my.clevelandclinic.org/health/diseases/6424-asthma"
+        }
+    },
+    adults: {
+        slow: {
+            name: "Diabeties",
+            imageUrl: "https://www.hiranandanihospital.org/public/uploads/blogs/1686748634.jpg",
+            link: "https://my.clevelandclinic.org/health/diseases/7104-diabetes"
+        },
+        fast: {
+            name: "BP",
+            imageUrl: "https://m.media-amazon.com/images/I/81thDpmpITL.jpg",
+            link: "https://www.mayoclinic.org/diseases-conditions/high-blood-pressure/diagnosis-treatment/drc-20373417"
+        }
+    }
+}
+
+export default data;
